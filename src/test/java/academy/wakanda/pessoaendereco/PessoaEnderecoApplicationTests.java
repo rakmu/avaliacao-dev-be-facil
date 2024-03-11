2f96d27f5c9e3fa6f1f7eb6f97fad581271061cc
Ramon Barbosa <ramon@ramonbarbosa.com>
2024-03-11 18:28:41 -0300

    inicia projeto

---
+++ src/test/java/academy/wakanda/pessoaendereco/PessoaEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.wakanda.pessoaendereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
