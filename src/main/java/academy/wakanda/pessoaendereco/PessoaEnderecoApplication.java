c91542213333f3c943d1e3ae32fa4594f68184e1
Ramon Barbosa <ramon@ramonbarbosa.com>
2024-03-11 19:13:26 -0300

    renomeia apçplication.propetis --> application.yml. teste okÂ!

--- src/main/java/academy/wakanda/pessoaendereco/PessoaEnderecoApplication.java
+++ src/main/java/academy/wakanda/pessoaendereco/PessoaEnderecoApplication.java
@@ -2,9 +2,19 @@ package academy.wakanda.pessoaendereco;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class PessoaEnderecoApplication {
+	@GetMapping
+	public String getHomeTeste() {
+		return "Pessoa Endereco - API Home";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PessoaEnderecoApplication.class, args);
