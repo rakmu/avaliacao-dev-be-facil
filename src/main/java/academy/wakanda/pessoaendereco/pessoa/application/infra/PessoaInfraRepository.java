d54ff3ea30766236c5faf910f31dac2fdf8949dd
Ramon Barbosa <ramon@ramonbarbosa.com>
2024-03-18 15:37:06 -0300

    implementa getTodasPessoas, teste ok!

--- src/main/java/academy/wakanda/pessoaendereco/pessoa/application/infra/PessoaInfraRepository.java
+++ src/main/java/academy/wakanda/pessoaendereco/pessoa/application/infra/PessoaInfraRepository.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @Log4j2
 @RequiredArgsConstructor
@@ -18,5 +20,12 @@ public class PessoaInfraRepository implements PessoaRepository {
         pessoaSpringDataJPARepository.save(pessoa);
         log.info("[finaliza] PessoaInfraRepository - salva");
         return pessoa;
+    }
+    @Override
+    public List<Pessoa> buscaTodasPessoas() {
+        log.info("[inicia] PessoaInfraRepository - buscaTodasPessoas");
+        List<Pessoa> todaPessoas = pessoaSpringDataJPARepository.findAll();
+        log.info("[finaliza] PessoaInfraRepository - buscaTodasPessoas");
+        return todaPessoas;
     }
 }
--- src/main/java/academy/wakanda/pessoaendereco/pessoa/application/repositoy/PessoaRepository.java
+++ src/main/java/academy/wakanda/pessoaendereco/pessoa/application/repositoy/PessoaRepository.java
@@ -2,6 +2,9 @@ package academy.wakanda.pessoaendereco.pessoa.application.repositoy;
 
 import academy.wakanda.pessoaendereco.pessoa.domain.Pessoa;
 
+import java.util.List;
+
 public interface PessoaRepository {
     Pessoa salva(Pessoa pessoa);
+    List<Pessoa> buscaTodasPessoas();
 }
